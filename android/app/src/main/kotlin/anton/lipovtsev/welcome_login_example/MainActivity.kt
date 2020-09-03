f654f61d478f49053091cfa7885d2f899ec7b766
AntonLipovtsev <lipovtsev.anton@yandex.by>
2020-09-03 13:05:41 +0300

    [CREATE]

---
+++ android/app/src/main/kotlin/anton/lipovtsev/welcome_login_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package anton.lipovtsev.welcome_login_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sign_in_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:welcome_login_example/constants.dart';
+
+class SignInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/background.jpeg"),
+                      fit: BoxFit.cover,
+                      alignment: Alignment.bottomCenter)),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "SIGN IN",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text(
+                        "SIGN UP",
+                        style: Theme.of(context).textTheme.button,
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 30),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 16),
+                          child: Icon(
+                            Icons.alternate_email,
+                            color: kPrimaryColor,
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration:
+                                InputDecoration(hintText: "Email Address"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Icon(
+                          Icons.lock,
+                          color: kPrimaryColor,
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(hintText: "Password"),
+                        ),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Row(
+                      children: [
+                        Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                    color: Colors.white.withOpacity(0.5))),
+                            padding: EdgeInsets.all(16),
+                            child: Icon(Icons.android,
+                                color: Colors.white.withOpacity(0.5))),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                    color: Colors.white.withOpacity(0.5))),
+                            padding: EdgeInsets.all(16),
+                            child: Icon(Icons.chat,
+                                color: Colors.white.withOpacity(0.5))),
+                        Spacer(),
+                        Container(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: kPrimaryColor),
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
